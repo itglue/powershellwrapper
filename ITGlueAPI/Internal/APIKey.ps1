1dbf0ac24135b2bcc22f3f5e846dace3e6a132dd
Matt Collins <5278184+TheMattCollins0@users.noreply.github.com>
2018-06-07 14:41:01 +0100

    Change to accept a API Key supplied as a parameter

--- ITGlueAPI/Internal/APIKey.ps1
+++ ITGlueAPI/Internal/APIKey.ps1
@@ -1,10 +1,23 @@
 function Add-ITGlueAPIKey {
+    [cmdletbinding()]
+    Param (
+        [Parameter(Mandatory = $false)]
+        [AllowEmptyString()]
+        [Alias('ApiKey')]
+        [string]$Api_Key
+    )
+    if ($Api_Key) {
+        $x_api_key = ConvertTo-SecureString $Api_Key -AsPlainText -Force 
+
+        Set-Variable -Name "ITGlue_API_Key"  -Value $x_api_key -Option ReadOnly -Scope global -Force
+    }
+    if (!$Api_Key) {
         Write-Host "Please enter your API key:"
         $x_api_key = Read-Host -AsSecureString
 
         Set-Variable -Name "ITGlue_API_Key"  -Value $x_api_key -Option ReadOnly -Scope global -Force
+    }
 }
-
 
 function Remove-ITGlueAPIKey {
     Remove-Variable -Name "ITGlue_API_Key"  -Force  
@@ -12,6 +25,7 @@ function Remove-ITGlueAPIKey {
 
 function Get-ITGlueAPIKey {
     $ITGlue_API_Key
+    
     Write-Host "Use Get-ITGlueAPIKey -Force to retrieve the unencrypted copy." -ForegroundColor "Red"
 }
 
