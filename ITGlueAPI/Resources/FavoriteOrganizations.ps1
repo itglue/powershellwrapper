eb4284ecc2c93433009c16fd6f46fed49e99e074
Adrian Wells <awells@syseng.com>
2018-07-11 14:33:27 -0400

    Added file to resolve Import-Module HRESULT: 0x8013104

---
+++ ITGlueAPI/Resources/FavoriteOrganizations.ps1
@@ -0,0 +1,98 @@
+function New-ITGlueFavoriteOrganizations {
+    Param (
+        [Parameter(Mandatory = $true)]
+        [Int64]$user_id,
+
+        [Parameter(Mandatory = $true)]
+        $data
+    )
+
+    $resource_uri = ('/users/{0}/relationships/favorite_organizations' -f $user_id)
+
+    $body = ConvertTo-Json -InputObject $data -Depth $ITGlue_JSON_Conversion_Depth
+
+    $ITGlue_Headers.Add('x-api-key', (New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList 'N/A', $ITGlue_API_Key).GetNetworkCredential().Password)
+    $rest_output = Invoke-RestMethod -method 'POST' -uri ($ITGlue_Base_URI + $resource_uri) -headers $ITGlue_Headers `
+        -body $body -ErrorAction Stop -ErrorVariable $web_error
+    $ITGlue_Headers.Remove('x-api-key') >$null # Quietly clean up scope so the API key doesn't persist
+
+    $data = @{}
+    $data = $rest_output 
+    return $data
+}
+
+function Get-ITGlueFavoriteOrganizations {
+    [CmdletBinding(DefaultParameterSetName = 'index')]
+    Param (
+        [Parameter(ParameterSetName = 'index')]
+        [Parameter(Mandatory = $true)]
+        [Int64]$user_id,
+
+        [Parameter(ParameterSetName = 'index')]
+        [ValidateSet( 'id', 'organization_id', 'organization_name', `
+                '-id', '-organization_id', '-organization_name')]
+        [String]$sort = '',
+
+        [Parameter(ParameterSetName = 'index')]
+        [Nullable[Int64]]$page_number = $null,
+
+        [Parameter(ParameterSetName = 'index')]
+        [Nullable[int]]$page_size = $null,
+
+        [Parameter(ParameterSetName = 'index')]
+        [ValidateSet('organization')]
+        [String]$include
+    )
+
+    $resource_uri = ('/users/{0}/relationships/favorite_organizations' -f $user_id)
+
+    if ($PSCmdlet.ParameterSetName -eq 'index') {
+        $body = @{
+            'sort'    = $sort
+            'include' = $include
+        }
+        if ($page_number) {
+            $body += @{'page[number]' = $page_number}
+        }
+        if ($page_size) {
+            $body += @{'page[size]' = $page_size}
+        }
+    }
+
+
+    $ITGlue_Headers.Add('x-api-key', (New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList 'N/A', $ITGlue_API_Key).GetNetworkCredential().Password)
+    $rest_output = Invoke-RestMethod -method 'GET' -uri ($ITGlue_Base_URI + $resource_uri) -headers $ITGlue_Headers `
+        -body $body -ErrorAction Stop -ErrorVariable $web_error
+    $ITGlue_Headers.Remove('x-api-key') >$null # Quietly clean up scope so the API key doesn't persist
+
+    $data = @{}
+    $data = $rest_output 
+    return $data
+}
+
+function Remove-ITGlueFavoriteOrganizations {
+    [CmdletBinding(SupportsShouldProcess, ConfirmImpact = 'High')]
+    Param (
+        [Parameter(Mandatory = $true)]
+        [Int64]$user_id,
+
+        [Parameter(Mandatory = $true)]
+        $data
+    )
+
+    $resource_uri = ('/users/{0}/relationships/favorite_organizations' -f $user_id)
+
+    $body = ConvertTo-Json -InputObject $data -Depth $ITGlue_JSON_Conversion_Depth
+
+    if ($pscmdlet.ShouldProcess($user_id)) {
+
+        $ITGlue_Headers.Add('x-api-key', (New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList 'N/A', $ITGlue_API_Key).GetNetworkCredential().Password)
+        $rest_output = Invoke-RestMethod -method 'DELETE' -uri ($ITGlue_Base_URI + $resource_uri) -headers $ITGlue_Headers `
+            -body $body -ErrorAction Stop -ErrorVariable $web_error
+        $ITGlue_Headers.Remove('x-api-key') >$null # Quietly clean up scope so the API key doesn't persist
+
+        $data = @{}
+        $data = $rest_output 
+        return $data
+    }
+}
