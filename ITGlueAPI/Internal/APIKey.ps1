28cb67b44d9e3be7c1499d7cb879eea36ceac68b
Adrian Wells <awells@syseng.com>
2018-09-25 14:06:57 -0400

    Fix Remove-ITGlueAPIKey and minor edits

--- ITGlueAPI/Internal/APIKey.ps1
+++ ITGlueAPI/Internal/APIKey.ps1
@@ -7,20 +7,20 @@ function Add-ITGlueAPIKey {
         [string]$Api_Key
     )
     if ($Api_Key) {
-        $x_api_key = ConvertTo-SecureString $Api_Key -AsPlainText -Force 
+        $x_api_key = ConvertTo-SecureString $Api_Key -AsPlainText -Force
 
-        Set-Variable -Name "ITGlue_API_Key"  -Value $x_api_key -Option ReadOnly -Scope global -Force
+        Set-Variable -Name "ITGlue_API_Key" -Value $x_api_key -Option ReadOnly -Scope global -Force
     }
     else {
         Write-Host "Please enter your API key:"
         $x_api_key = Read-Host -AsSecureString
 
-        Set-Variable -Name "ITGlue_API_Key"  -Value $x_api_key -Option ReadOnly -Scope global -Force
+        Set-Variable -Name "ITGlue_API_Key" -Value $x_api_key -Option ReadOnly -Scope global -Force
     }
 }
 
 function Remove-ITGlueAPIKey {
-    Remove-Variable -Name "ITGlue_API_Key"  -Force  
+    Remove-Variable -Name "ITGlue_API_Key" -Scope global -Force
 }
 
 function Get-ITGlueAPIKey {
