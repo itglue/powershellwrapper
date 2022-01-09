7451eab8ed91d2a1425f6c9b98426c1a54fe7e83
Celerium <celerium@celerium.org>
2022-01-09 16:12:14 -0700

    Added support for ITGlue's Australia data center and added help comments to the main Add-ITGlueBaseURI function to help out any new user to the powershell wrapper.

--- ITGlueAPI/Internal/BaseURI.ps1
+++ ITGlueAPI/Internal/BaseURI.ps1
@@ -1,15 +1,49 @@
 function Add-ITGlueBaseURI {
+<#
+.SYNOPSIS
+    Sets the base URI for the ITGlue API connection.
+
+.DESCRIPTION
+    The Add-ITGlueBaseURI cmdlet sets the base URI which is later used to construct the full URI for all API calls to ITGlue.
+
+.PARAMETER base_uri
+    Define the base URI for the ITGlue API connection using ITGlue's URI's or a custom URI.
+
+.PARAMETER data_center
+    ITGlue's URI connection point that can be one of the predefined data centers. The accepted values for this parameter are:
+    [ US , EU , AU ]
+    US = https://api.itglue.com
+        EU = https://api.eu.itglue.com
+        AU = https://api.au.itglue.com
+
+.EXAMPLE
+    Add-ITGlueBaseURI
+
+    The base URI will use https://api.itglue.com which is ITGlue's US data center.
+
+.EXAMPLE
+    Add-ITGlueBaseURI -base_uri http://myapi.gateway.example.com
+
+    A custom API gateway of http://myapi.gateway.example.com will be used for all API calls to ITGlue.
+
+.EXAMPLE
+    Add-ITGlueBaseURI -data_center AU
+
+    The base URI will be https://api.au.itglue.com which is ITGlue's Australia data center.
+
+#>
+
     [cmdletbinding()]
     Param (
         [parameter(ValueFromPipeline)]
         [string]$base_uri = 'https://api.itglue.com',
 
         [Alias('locale','dc')]
-        [ValidateSet( 'US', 'EU')]
+        [ValidateSet( 'US', 'EU', 'AU')]
         [String]$data_center = ''
     )
 
-    # Trim superflous forward slash from address (if applicable)
+    # Trim superfluous forward slash from address (if applicable)
     if($base_uri[$base_uri.Length-1] -eq "/") {
         $base_uri = $base_uri.Substring(0,$base_uri.Length-1)
     }
@@ -17,6 +51,7 @@ function Add-ITGlueBaseURI {
     switch ($data_center) {
         'US' {$base_uri = 'https://api.itglue.com'}
         'EU' {$base_uri = 'https://api.eu.itglue.com'}
+        'AU' {$base_uri = 'https://api.au.itglue.com'}
         Default {}
     }
 
@@ -24,7 +59,7 @@ function Add-ITGlueBaseURI {
 }
 
 function Remove-ITGlueBaseURI {
-    Remove-Variable -Name "ITGlue_Base_URI" -Scope global -Force 
+    Remove-Variable -Name "ITGlue_Base_URI" -Scope global -Force
 }
 
 function Get-ITGlueBaseURI {
