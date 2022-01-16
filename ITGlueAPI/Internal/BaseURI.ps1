538705b02eee3ff2f1875096865200746e676f63
Celerium <celerium@celerium.org>
2022-01-16 13:53:29 -0700

    Removed PSHelp comment to work on them in a branch first

--- ITGlueAPI/Internal/BaseURI.ps1
+++ ITGlueAPI/Internal/BaseURI.ps1
@@ -1,38 +1,4 @@
 function Add-ITGlueBaseURI {
-<#
-.SYNOPSIS
-    Sets the base URI for the ITGlue API connection.
-
-.DESCRIPTION
-    The Add-ITGlueBaseURI cmdlet sets the base URI which is later used to construct the full URI for all API calls to ITGlue.
-
-.PARAMETER base_uri
-    Define the base URI for the ITGlue API connection using ITGlue's URI's or a custom URI.
-
-.PARAMETER data_center
-    ITGlue's URI connection point that can be one of the predefined data centers. The accepted values for this parameter are:
-    [ US , EU , AU ]
-    US = https://api.itglue.com
-        EU = https://api.eu.itglue.com
-        AU = https://api.au.itglue.com
-
-.EXAMPLE
-    Add-ITGlueBaseURI
-
-    The base URI will use https://api.itglue.com which is ITGlue's US data center.
-
-.EXAMPLE
-    Add-ITGlueBaseURI -base_uri http://myapi.gateway.example.com
-
-    A custom API gateway of http://myapi.gateway.example.com will be used for all API calls to ITGlue.
-
-.EXAMPLE
-    Add-ITGlueBaseURI -data_center AU
-
-    The base URI will be https://api.au.itglue.com which is ITGlue's Australia data center.
-
-#>
-
     [cmdletbinding()]
     Param (
         [parameter(ValueFromPipeline)]
