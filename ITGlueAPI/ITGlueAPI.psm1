ff667ac3480592007158b3b2db96cccc3151a500
David Haymond <david@davidhaymond.dev>
2022-12-07 12:18:25 -0800

    Explicitly select UTF-8 encoding for API requests

--- ITGlueAPI/ITGlueAPI.psm1
+++ ITGlueAPI/ITGlueAPI.psm1
@@ -1,5 +1,5 @@
 $ITGlue_Headers = New-Object "System.Collections.Generic.Dictionary[[String],[String]]"
-$ITGlue_Headers.Add("Content-Type", 'application/vnd.api+json')
+$ITGlue_Headers.Add("Content-Type", 'application/vnd.api+json; charset=utf-8')
 
 Set-Variable -Name "ITGlue_Headers"  -Value $ITGlue_Headers -Scope global
 
